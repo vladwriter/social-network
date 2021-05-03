75a1bea7f6cd554d9aff3d231408aef729837de1
vladwriter <vladwriter96@gmail.com>
2021-05-03 11:19:21 +0300

    [^]Fix bug in authentication

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = (props) => {
         <header className={s.header}>
         <img  src = "https://www.logodesign.net/images/illustration-logo.png" alt='logo'/>
         <div className={s.loginBlock}>
-          {PaymentResponse.isAuth ? props.login:
+          {props.isAuth ? props.login :
           <NavLink to={'/login'}>Login</NavLink> }
         </div>
       </header>
