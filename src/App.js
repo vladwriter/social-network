ca6d9040b8dad7c8c05c6a86ca4f9f387cd462fe
vladwriter <vladwriter96@gmail.com>
2021-05-03 09:41:37 +0300

    [+] Fix path in Profile

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = (props) => {
                     store = { props.store }
                     /> } / >
                     <
-                    Route path = '/profile'
+                    Route path = '/profile/:userId?'
                     render = {
                         () => < ProfileContainer store = { props.store }
                         /> } / >
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Profile from './Profile';
 import * as axios from 'axios';
+import {withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {setUserProfile} from "../../redux/profile-reducer";
 
 class ProfileContainer extends React.Component {
 
   componentDidMount() {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+    let userId = this.props.match.params.userId;
+    if (!userId){
+      userId = 2;
+    }
+    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then(response => {
         this.props.setUserProfile(response.data);
 
@@ -26,4 +31,6 @@ const mapStateToProps = (state) => ({
   profile: state.profilePage.profile
 })
 
-export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);
+const WithUrlDataContainerComponent = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, {setUserProfile})(WithUrlDataContainerComponent);
