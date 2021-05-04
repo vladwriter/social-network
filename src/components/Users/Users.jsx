c28bd9953f64166c47155a53ad4e3455aab3374b
vladwriter <vladwriter96@gmail.com>
2021-05-04 09:31:04 +0300

    [^]Update follow/unfollow in Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,50 +1,73 @@
 import React from 'react';
 import styles from './users.module.css';
 import userPhoto from '../../../src/assets/img/defaultUser.png';
+import * as axios from 'axios';
 import { NavLink } from 'react-router-dom';
 
 const Users = (props) => {
 
-    let pagesCount = Math.ceil(props.totalUsersCount/props.pageSize);
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
     let pages = [];
-    for(let i=1; i<=pagesCount; i++){
+    for (let i = 1; i <= pagesCount; i++) {
         pages.push(i)
     }
 
 
     return <div>
-            <div className={styles.pageCount}>
-                {pages.map(p=>{
-                   return <span className={props.currentPage === p && styles.selectedPage}
-                   onClick={(e)=>{props.onPageChanged(p);}}>{p} </span>
-                })}
-            </div>
+        <div className={styles.pageCount}>
+            {pages.map(p => {
+                return <span className={props.currentPage === p && styles.selectedPage}
+                    onClick={(e) => { props.onPageChanged(p); }}>{p} </span>
+            })}
+        </div>
         {
-          props.users.map(u =><div key ={u.id}>
-              <span>
-                  <div>
-                      <NavLink to={'/profile/' + u.id}>
-                         <img src={u.photos.small !=null ? u.photos.small: userPhoto} alt="" className={styles.userPhoto}/>
-                      </NavLink>                 
-                  </div>
-                  <div>
-                      {u.followed 
-                      ? <button onClick = {()=>{props.unfollow(u.id)}}>Unfollow</button>
-                      : <button onClick = {()=>{props.follow(u.id)}}>Follow</button>}
-                  </div>
-              </span>
-              <span>
-                  <span>
-                      <div>{u.name}</div>
-                      <div>{u.status}</div>
-                  </span>
-                  <span>
+            props.users.map(u => <div key={u.id}>
+                <span>
+                    <div>
+                        <NavLink to={'/profile/' + u.id}>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="" className={styles.userPhoto} />
+                        </NavLink>
+                    </div>
+                    <div>
+                        {u.followed
+                            ? <button onClick={() => {
+                                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, { 
+                                    withCredentials: true,
+                                    headers:{
+                                        'API-KEY': 'b7104834-b6a6-4f83-b432-6d6cfda2a81f'
+                                    } })
+                                .then(response => {
+                                        if (response.data.resultCode === 0) {
+                                            props.unfollow(u.id);
+                                        }
+                                       });
+                            }}>Unfollow</button>
+                            : <button onClick={() => {
+                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, { 
+                                    withCredentials: true,
+                                    headers:{
+                                    'API-KEY': 'b7104834-b6a6-4f83-b432-6d6cfda2a81f'
+                                } })
+                                    .then(response => {
+                                        if (response.data.resultCode === 0) {
+                                            props.follow(u.id);
+                                        }
+                                    });
+                                    }}>Follow</button>}
+                    </div>
+                </span>
+                <span>
+                    <span>
+                        <div>{u.name}</div>
+                        <div>{u.status}</div>
+                    </span>
+                    <span>
                         <div>{'u.location.country'}</div>
                         <div>{'u.location.city'}</div>
-                  </span>
-              </span>
-          </div>)
+                    </span>
+                </span>
+            </div>)
         }
     </div>
 }
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -8,7 +8,8 @@ import Preloader from '../common/Preloader/Preloader';
 class UsersContainer extends React.Component{
     componentDidMount(){
         this.props.toggleIsFetching(true);
-             axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response =>{
+             axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`,
+                    {withCredentials: true}).then(response =>{
                 this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items);
                 this.props.setTotalUsersCount(response.data.totalCount);
@@ -18,7 +19,8 @@ class UsersContainer extends React.Component{
         onPageChanged =(pageNumber) =>{
             this.props.setCurrentPage(pageNumber);
             this.props.toggleIsFetching(true);
-            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response =>{
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, 
+            {withCredentials:true}).then(response =>{
                 this.props.toggleIsFetching(false);
                 this.props.setUsers(response.data.items);
              } );
