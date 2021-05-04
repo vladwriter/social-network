2c7238b04902987553a837677ccd0264d824fea8
vladwriter <vladwriter96@gmail.com>
2021-05-04 10:19:59 +0300

    [+] Create api.js

---
+++ src/API/api.js
@@ -0,0 +1,17 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': 'b7104834-b6a6-4f83-b432-6d6cfda2a81f'
+    }
+
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => response.data);
+    }
+}
