9f6bde7db35cfb8020d61703c9a79a3049333a19
vladwriter <vladwriter96@gmail.com>
2021-05-29 11:52:45 +0300

    [^] Update status

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,8 +1,10 @@
 import { usersAPI } from '../API/api';
+import { profileAPI } from '../API/api';
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET_USER_PROFILE';
+const SET_STATUS = 'SET_STATUS';
 
 let initialState = {
     posts: [
@@ -13,7 +15,8 @@ let initialState = {
         { id: 5, text: 'Pray for you', likesCount: 87 }
     ],
     newPostText: 'Write new post!',
-    profile: null
+    profile: null,
+    status: ""
 }
 
 const profileReducer = (state = initialState, action) => {
@@ -32,6 +35,10 @@ const profileReducer = (state = initialState, action) => {
             return {...state,
                 newPostText: action.newText
             };
+        case SET_STATUS:
+            return {...state,
+                status: action.status
+            };
         case SET_USER_PROFILE:
             return {...state,
                 profile: action.profile
@@ -43,11 +50,26 @@ const profileReducer = (state = initialState, action) => {
 export const addPostActionCreator = () => ({ type: ADD_POST })
 export const updateNewPostTextActionCreator = (text) =>
     ({ type: UPDATE_NEW_POST_TEXT, newText: text })
-
+export const setStatus = (status) => ({ type: SET_STATUS, status })
 const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile })
 export const getUserProfile = (userId) => (dispatch) => {
     usersAPI.getProfile(userId).then(response => {
         dispatch(setUserProfile(response.data));
     });
 }
+
+export const getStatus = (userId) => (dispatch) => {
+    profileAPI.getStatus(userId).then(response => {
+        dispatch(setStatus(response.data));
+    });
+}
+
+export const updateStatus = (status) => (dispatch) => {
+    profileAPI.updateStatus(status).then(response => {
+        if(response.data.resultCode === 0){
+            dispatch(setStatus(status));
+        }
+    });
+}
+
 export default profileReducer;
